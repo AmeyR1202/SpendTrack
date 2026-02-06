d74d3b067d013397b5cb57cc2899e71d9ab3ca9e
AmeyR1202 <ameyrakhe01@gmail.com>
2026-02-06 17:56:20 +0530

    UI(EnterNameScreen): Refactored the UI of EnterNameScreen

--- lib/feature/expense/presentation/enter_name/enter_name_screen.dart
+++ lib/feature/expense/presentation/enter_name/enter_name_screen.dart
@@ -17,6 +17,7 @@ class EnterNameScreen extends StatelessWidget {
         if (state.status == Status.success) {
           context.go('/dashboard');
         }
+
         if (state.status == Status.error && state.errorMessage != null) {
           ScaffoldMessenger.of(
             context,
@@ -24,68 +25,126 @@ class EnterNameScreen extends StatelessWidget {
         }
       },
       child: Scaffold(
-        appBar: AppBar(title: Text('Enter your name')),
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              BlocBuilder<EnterNameBloc, EnterNameState>(
-                buildWhen: (previous, current) => previous.name != current.name,
-                builder: (context, state) {
-                  return TextField(
-                    onChanged: (value) {
-                      context.read<EnterNameBloc>().add(NameChanged(value));
-                    },
-                    decoration: const InputDecoration(labelText: 'Name'),
-                  );
-                },
-              ),
-              const SizedBox(height: 24),
-              BlocBuilder<EnterNameBloc, EnterNameState>(
-                buildWhen: (previous, current) =>
-                    previous.status != current.status,
-                builder: (context, state) {
-                  return SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: state.status == Status.loading
-                          ? null
-                          : () {
-                              context.read<EnterNameBloc>().add(SavePressed());
-                            },
-                      child: state.status == Status.loading
-                          ? const CircularProgressIndicator()
-                          : const Text('Continue'),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Let us know what we should call you?',
+                          style: Theme.of(context).textTheme.bodyLarge
+                              ?.copyWith(
+                                fontSize: 32,
+                                fontWeight: FontWeight.w800,
+                              ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        BlocBuilder<EnterNameBloc, EnterNameState>(
+                          buildWhen: (p, c) => p.name != c.name,
+                          builder: (context, state) {
+                            return TextField(
+                              // autofocus: true,
+                              onChanged: (value) {
+                                context.read<EnterNameBloc>().add(
+                                  NameChanged(value),
+                                );
+                              },
+                              decoration: InputDecoration(
+                                hintText: 'Enter your name',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: const BorderSide(
+                                    color: Color.fromRGBO(
+                                      0,
+                                      0,
+                                      0,
+                                      0.3,
+                                    ), // enabled (not focused)
+                                    width: 1,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: const BorderSide(
+                                    color: Color.fromRGBO(
+                                      0,
+                                      0,
+                                      0,
+                                      0.1,
+                                    ), // focused
+                                    width: 1,
+                                  ),
+                                ),
+                              ),
+                              cursorColor: Colors.black,
+                            );
+                          },
+                        ),
+                      ],
                     ),
-                  );
-                },
-              ),
-            ],
+                  ),
+                ),
+
+                BlocBuilder<EnterNameBloc, EnterNameState>(
+                  buildWhen: (p, c) => p.name != c.name,
+                  builder: (context, state) {
+                    final isEnabled = state.name.trim().isNotEmpty;
+
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 52,
+                        child: ElevatedButton(
+                          onPressed: isEnabled
+                              ? () {
+                                  context.read<EnterNameBloc>().add(
+                                    SavePressed(),
+                                  );
+                                }
+                              : null,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: isEnabled
+                                ? Colors.blue
+                                : Colors.transparent,
+                            disabledBackgroundColor: const Color.fromARGB(
+                              255,
+                              0,
+                              0,
+                              0,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                          ),
+                          child: const Text(
+                            'Continue',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-// class EnterNameScreen extends StatelessWidget {
-//   const EnterNameScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocListener<EnterNameBloc, EnterNameState>(
-//       listenWhen: (previous, current) => previous.status != current.status,
-//       listener: (context, state) {
-//         if (state.status == Status.success) {
-//           context.go('/dashboard');
-//         }
-//         if (state.status == Status.error && state.errorMessage != null) {
-//           ScaffoldMessenger.of(
-//             context,
-//           ).showSnackBar(SnackBar(content: Text(state.errorMessage!)));
-//         }
-//       },
-//       child: Scaffold(),
-//     );
-//   }
-// }
