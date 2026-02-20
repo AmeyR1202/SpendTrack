d4518ccf99c18e0af87793187d0c4488afc2c258
AmeyR1202 <ameyrakhe01@gmail.com>
2026-02-20 22:35:36 +0530

    Added app logo on splash screen

--- lib/feature/expense/presentation/splash/splash_screen.dart
+++ lib/feature/expense/presentation/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:spend_wise/core/state/status.dart';
+import 'package:spend_wise/core/theme/app_colors.dart';
 import 'package:spend_wise/feature/expense/presentation/splash/bloc/splash_bloc.dart';
 import 'package:spend_wise/feature/expense/presentation/splash/bloc/splash_event.dart';
 import 'package:spend_wise/feature/expense/presentation/splash/bloc/splash_state.dart';
@@ -29,14 +30,38 @@ class _SplashScreenState extends State<SplashScreen> {
           if (state.isUserPresent) {
             context.go('/dashboard');
           } else {
-            context.go('/enter-name');
+            context.go('/');
           }
           if (state.status == Status.error) {
             // Optional: log or show fallback UI later
           }
         }
       },
-      child: const Scaffold(body: Center(child: CircularProgressIndicator())),
+      child: const Scaffold(
+        backgroundColor: AppColors.background,
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Spacer(flex: 3),
+
+              Image(
+                image: AssetImage('assets/icon/spend_wise.png'),
+                width: 140,
+              ),
+
+              Spacer(flex: 2),
+
+              SizedBox(
+                width: 180,
+                child: LinearProgressIndicator(minHeight: 3),
+              ),
+
+              Spacer(flex: 1),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
